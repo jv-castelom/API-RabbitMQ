6a91f3ecd11a0406515b11e7b9eb88856e5d9f57
jv-castelom <vitorcastelo.m3@gmail.com>
2021-03-06 16:28:20 -0300

    Organize Project in Multi Layer Architecture

---
+++ Core/Models/Entities/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Models.Entities
+{
+    public class Customer : Entity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CPF { get; set; }
+        public string Phone { get; set; }
+    }
+}
