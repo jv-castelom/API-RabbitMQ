ffc35e80bb6ce458f2578100fb691e7eae79ff27
jv-castelom <vitorcastelo.m3@gmail.com>
2021-03-07 12:35:58 -0300

    Create Customer Controller

---
+++ AT_API_MS_Rabbit/Controllers/CustomerController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Models.Entities;
+using Data;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Requests.Customers;
+
+namespace AT_API_MS_Rabbit.Controllers
+{
+    public class CustomerController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> RegisterCustomer(RegisterCustomer request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(request);
+            }
+                
+
+            var customer = new Customer()
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                CPF = request.CPF,
+                Phone = request.Phone
+            };
+
+            var connection = new MongoAcess();
+            await connection.Customers.InsertOneAsync(customer);
+            return View();
+        }
+    }
+}
