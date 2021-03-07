a1283ca8692b507b12bfeb0c9be91bf64b85138a
jv-castelom <vitorcastelo.m3@gmail.com>
2021-03-07 12:34:33 -0300

    Create Register Customer Request

---
+++ Shared/Requests/Customers/RegisterCustomer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Shared.Requests.Customers
+{
+    public class RegisterCustomer
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "CPF")]
+        public string CPF { get; set; }
+        
+        [DataType(DataType.Text)]
+        [Display(Name = "Phone")]
+        public string Phone { get; set; }
+    }
+}
