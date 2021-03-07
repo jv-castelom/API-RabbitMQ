055b24f54fda0f571d025cd37bd928f94960e269
jv-castelom <vitorcastelo.m3@gmail.com>
2021-03-07 12:35:07 -0300

    Create Acess Class To MongoDB

---
+++ Data/MongoAcess.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Models.Entities;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Data
+{
+    public class MongoAcess
+    {
+        public const string CONNECTION_STRING = "mongodb://localhost:27017";
+        public const string DATABASE = "AT_API";
+        public const string COLLECTION = "Customers";
+        public static IMongoClient _client { get; }
+        public static IMongoDatabase _database { get; }
+
+        static MongoAcess()
+        {
+            _client = new MongoClient(CONNECTION_STRING);
+            _database = _client.GetDatabase(DATABASE);
+        }
+
+        public IMongoCollection<Customer> Customers
+        {
+            get { return _database.GetCollection<Customer>(COLLECTION); }
+        }
+    }
+}
